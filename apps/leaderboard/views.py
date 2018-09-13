cd0233ca5daa8637ebf6f6a387952fc0e96a6bc8
Noelle <noelle.teh@gmail.com>
2018-09-13 15:27:44 -0700

    fixed leaderboard count bug

--- apps/leaderboard/views.py
+++ apps/leaderboard/views.py
@@ -4,7 +4,8 @@ from apps.error_log_mgmt.models import Suggestion, StudentError
 from apps.user_mgmt.models import CustomUser
 
 def index(request):
-    annotated_users = CustomUser.objects.annotate(num_errors=Count('errors_shared'), num_suggestions=Count('suggestions_given'))
+    annotated_users = CustomUser.objects.annotate(num_errors=Count('errors_shared'))
     users_errors = annotated_users.filter(num_errors__gt=0).order_by('-num_errors')
+    annotated_users = CustomUser.objects.annotate(num_suggestions=Count('suggestions_given'))
     users_suggestions = annotated_users.filter(num_suggestions__gt=0).order_by('-num_suggestions')
     return render(request, "leaderboard/index.html", context = {'errors': users_errors, 'suggestions': users_suggestions})
